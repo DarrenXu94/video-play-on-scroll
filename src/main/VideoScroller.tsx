f39f464a6aad5d05e0afbb23ec6ee91fe0d0a229
Darren Xu <darrenxu1994@gmail.com>
2021-09-13 20:46:01 +1000

    Video preload

--- src/main/VideoScroller.tsx
+++ src/main/VideoScroller.tsx
@@ -12,7 +12,18 @@ export interface VideoScrollerProps {}
 export default function VideoScroller({}: VideoScrollerProps) {
   const videoRef = useRef<any>();
 
+  function onSuccess(url) {
+    console.log(url);
+    videoRef.current.src = url;
+  }
+
+  function onProgress() {}
+
+  function onError() {}
+
   useEffect(() => {
+    prefetch_file(video, onSuccess, onProgress, onError);
+
     setInterval(() => {
       delay += (scrollpos - delay) * accelamount;
 
@@ -45,12 +56,12 @@ export default function VideoScroller({}: VideoScrollerProps) {
                       opacity: "0.1",
                     },
                   }}
-                  totalProgress={progress <= 0.5 ? 0 : (progress - 0.5) * 2}
+                  //   totalProgress={progress <= 0.5 ? 0 : (progress - 0.5) * 2}
                   paused
                 >
                   <h1>Scroll Down</h1>
 
-                  <video src={video} ref={videoRef} preload={"auto"} />
+                  <video ref={videoRef} />
                 </Tween>
               </div>
             );
@@ -91,4 +102,41 @@ export default function VideoScroller({}: VideoScrollerProps) {
       <div style={{ height: "100vh" }}></div> */}
     </div>
   );
+}
+
+function prefetch_file(
+  url,
+  fetched_callback,
+  progress_callback,
+  error_callback
+) {
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", url, true);
+  xhr.responseType = "blob";
+
+  xhr.addEventListener(
+    "load",
+    function () {
+      if (xhr.status === 200) {
+        var URL = window.URL || window.webkitURL;
+        var blob_url = URL.createObjectURL(xhr.response);
+        fetched_callback(blob_url);
+      } else {
+        error_callback();
+      }
+    },
+    false
+  );
+
+  var prev_pc = 0;
+  xhr.addEventListener("progress", function (event) {
+    if (event.lengthComputable) {
+      var pc = Math.round((event.loaded / event.total) * 100);
+      if (pc != prev_pc) {
+        prev_pc = pc;
+        progress_callback(pc);
+      }
+    }
+  });
+  xhr.send();
 }
